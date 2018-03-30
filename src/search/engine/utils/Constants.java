96219438cfebca9e5b70343bdaab277b89611e94
Ibrahim Radwan <i.radwan1996@gmail.com>
2018-03-30 03:03:29 +0200

    STOP_WORDS contained I -> Repalced by i

--- src/search/engine/utils/Constants.java
+++ src/search/engine/utils/Constants.java
@@ -97,7 +97,7 @@ public class Constants {
     //
 
     public static final String[] STOP_WORDS = {
-            "I", "a", "about", "an", "are", "as", "at", "be", "by", "com", "for",
+            "i", "a", "about", "an", "are", "as", "at", "be", "by", "com", "for",
             "from", "how", "in", "is", "it", "of", "on", "or", "that", "the",
             "this", "to", "was", "what", "when", "where", "who", "will",
             "with", "the", "www", "can", "and"
