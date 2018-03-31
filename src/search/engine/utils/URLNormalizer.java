50256e282f342899588377b0afd5b07d016ed264
OmarBazaraa <omar_bazaraa@hotmail.com>
2018-04-01 00:23:50 +0200

    Minor change.

--- src/search/engine/utils/URLNormalizer.java
+++ src/search/engine/utils/URLNormalizer.java
@@ -4,7 +4,6 @@ import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
